1b149f2e5f8a1232758bebfd1badd9f98e78f401
EkWsR <edieskyr@gmail.com>
2025-07-30 10:53:58 -0400

    cambios en la nueva rama

--- test_app.py
+++ test_app.py
@@ -1,4 +1,11 @@
-from app import suma
+from app import suma, resta
+
+
 def test_suma():
-    assert suma(2,3) == 5
-    assert suma(0,0) == 0
+    assert suma(2, 3) == 5
+    assert suma(0, 0) == 0
+
+
+def test_resta():
+    assert resta(6, 3) == 3
+    assert resta(0, 0) == 0
